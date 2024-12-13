f08b9d636b221f247eb38780a68b436565278e5c
Behzad <bh.ahmady@gmail.com>
2024-12-13 11:09:50 +0330

    [ADD] dynamic route example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,31 @@
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
 
-export default function Home() {
-  return <h1>شروع بوت کمپ!</h1>
+export default async function Home() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts: Post[] = await res.json();
+
+  return (
+    <main>
+      <h1>شروع بوت کمپ!</h1>
+
+      {/* It's better to be "ltr", because of English content*/}
+      <p dir="ltr">Post List</p>
+
+      <ul dir="ltr">
+        {posts.map((post: Post) => (
+          <>
+            <li key={post.id}>Title: {post.title}</li>
+            <ul>
+              <li>{post.body}</li>
+            </ul>
+          </>
+        ))}
+      </ul>
+    </main>
+  );
 }
