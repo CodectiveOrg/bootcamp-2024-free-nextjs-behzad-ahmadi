451b30d75cbfde9ee136b9bab141cdc64e3f4001
Behzad Haji Ahmadi <bh.ahmady@gmail.com>
2024-12-19 11:20:17 +0330

    [OMIT] unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,3 @@
-type Post = {
-  id: number;
-  title: string;
-  body: string;
-};
-
 export default async function Home() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const posts: Post[] = await res.json();
-
-  return (
-    <main>
-      <h1>شروع بوت کمپ!</h1>
-
-      {/* It's better to be "ltr", because of English content*/}
-      <p dir="ltr">Post List</p>
-
-      <ul dir="ltr">
-        {posts.map((post: Post) => (
-          <>
-            <li key={post.id}>Title: {post.title}</li>
-            <ul>
-              <li>{post.body}</li>
-            </ul>
-          </>
-        ))}
-      </ul>
-    </main>
-  );
+  return <h1>شروع بوت کمپ!</h1>;
 }
