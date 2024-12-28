f835f92da976aebc53f18bcf9b6ff3e6e67aec7d
Behzad Haji Ahmadi <bh.ahmady@gmail.com>
2024-12-28 11:15:55 +0330

    [ADD] not-found WIP

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <div className="content"></div>
+      <div className="image-container"></div>
+    </div>
+  );
+}
