e5eb084ceb24627b4b439d67b1d6c3e5664bcb8d
Behzad <bh.ahmady@gmail.com>
2025-01-23 00:49:02 +0330

    Added CommentRatings MockData

---
+++ src/mock/commentRatings.ts
@@ -0,0 +1,14 @@
+export const CommentRatings = [
+  {
+    label: 'برخورد مناسب پزشک',
+    score: 4.7,
+  },
+  {
+    label: 'توضیح پزشک در هنگام ویزیت',
+    score: 4.6,
+  },
+  {
+    label: 'مهارت و تخصص پزشک',
+    score: 4.6,
+  },
+];
