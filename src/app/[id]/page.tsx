c66663f9508cdbb850c44176312c182636f62968
Behzad <bh.ahmady@gmail.com>
2025-01-23 00:49:28 +0330

    Fixed typo

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './style.module.css';
 import DoctorCard from '@/app/[id]/ui/doctorCard';
+import CommentsCard from '@/app/[id]/ui/commentsCard';
 
 interface Props {
   params: { id: string };
@@ -10,7 +11,8 @@ export default async function DoctorDetails({ params }: Props) {
   return (
     <div className={styles.container}>
       <div className={styles['right-col']}>
-        <DoctorCard badges={['دکتور', 'پزشک']} />
+        <DoctorCard badges={['دکتر', 'پزشک']} />
+        <CommentsCard />
       </div>
       <div className={styles['left-col']}>L</div>
     </div>
