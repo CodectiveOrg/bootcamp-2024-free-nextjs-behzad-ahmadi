ba376e8ff6f2195fd35ed32dff44a61964c8d5c3
Behzad <bh.ahmady@gmail.com>
2025-01-31 04:14:55 +0330

    Update avatar section style

--- src/app/[id]/ui/commentsCard/commentSection/ui/userInfo/index.tsx
+++ src/app/[id]/ui/commentsCard/commentSection/ui/userInfo/index.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import styles from './style.module.css';
+import clsx from 'clsx';
+import { formatDateToRelativePersian } from '@/lib/helper';
 
 type Props = { name: string; id: number; status: string; date: string };
 
-export default function UserInfo({ name, id, status }: Props) {
+export default function UserInfo({ name, id, status, date }: Props) {
   return (
-    <div>
-      <div className={styles.avatar}>{name.slice(0, 1)}</div>
+    <div className={styles.container}>
+      <div
+        className={clsx(
+          styles.avatar,
+          styles[`bg-color-${Math.trunc(Math.random() * 5 + 1)}`],
+        )}
+      >
+        {name?.slice(0, 1)}
+      </div>
 
-      <div>
-        <div>
+      <div className={styles.info}>
+        <div className={styles.r1}>
           <span className={styles.name}>{name}</span>
-          <span className={styles.status}>{status}</span>
+          <span className={styles.status}>{status}و</span>
         </div>
 
         <div>
-          <span className={styles.date}>{date}</span>
+          <span className={styles.date}>
+            {formatDateToRelativePersian(date)}
+          </span>
         </div>
       </div>
     </div>
