87a50dea3b6f18f7170de8b58e8eb651ffaedc25
Behzad <bh.ahmady@gmail.com>
2025-02-02 19:09:23 +0330

    Translate status

--- src/app/[id]/ui/commentsCard/commentSection/ui/userInfo/index.tsx
+++ src/app/[id]/ui/commentsCard/commentSection/ui/userInfo/index.tsx
@@ -8,6 +8,17 @@ type Props = { name: string; id: number; status: string; date: string };
 export default function UserInfo({ name, id, status, date }: Props) {
   const color = useMemo(() => Math.trunc(Math.random() * 5 + 1), [name]);
 
+  const translatedStatus = useMemo(() => {
+    switch (status) {
+      case 'visited':
+        return 'ویزیت شده';
+      case 'not_visited':
+        return 'ویزیت نشده';
+      default:
+        break;
+    }
+  }, [status]);
+
   return (
     <div className={styles.container}>
       <div className={clsx(styles.avatar, styles[`bg-color-${color}`])}>
@@ -17,7 +28,7 @@ export default function UserInfo({ name, id, status, date }: Props) {
       <div className={styles.info}>
         <div className={styles.r1}>
           <span className={styles.name}>{name}</span>
-          {status && <span className={styles.status}>{status}</span>}
+          {status && <span className={styles.status}>{translatedStatus}</span>}
         </div>
 
         <div>
