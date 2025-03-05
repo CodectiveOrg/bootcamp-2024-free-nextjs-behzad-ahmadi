6f231868138278292c0766f50835b2bdc2dcff34
Behzad <bh.ahmady@gmail.com>
2025-03-06 02:18:33 +0330

    Rename component folder name to PascalCase

--- src/ui/loading/loading.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './style.module.css';
-
-export default function Loading(): React.ReactNode {
-  return (
-    <div className={styles.loading}>
-      <span className={styles.dots}></span>
-    </div>
-  );
-}
